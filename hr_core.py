6748c17a0509d6c95a116d239c30eb6994b10f84
Sam Schreiber <srschreiber@github.com>
2023-02-13 10:52:56 -0800

    run local script to validate

---
+++ hr_core.py
@@ -0,0 +1,60 @@
+import os
+import logging
+import requests as http
+import sys
+
+cwd = os.path.abspath(os.getcwd())
+include_path = f"{cwd}"
+sys.path.append(include_path)
+
+logger = logging.getLogger("HrCore")
+logging.basicConfig(level=logging.INFO)
+class HrCore:
+    def __init__(self) -> None:
+
+        self.hr_core_api_people_cid = os.getenv("HR_CORE_API_HARD_DELETE_CLIENT_ID")
+        self.client_id = os.getenv("MSFT_TENANT_CLIENT_ID")
+        self.client_secret = os.getenv("MSFT_TENANT_CLIENT_SECRET")
+        self.azure_tenant_id = os.getenv("MSFT_AZURE_TENANT_ID")
+        self.base_url = os.getenv("HR_CORE_API_URL")
+
+
+    def get_alias_by_employee_id(self, employee_id):
+        token = self.__get_hr_api_bearer_token(self.hr_core_api_people_cid)
+        headers = {
+            "Authorization": token,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }
+        url = f"{self.base_url}/Person/{employee_id}"
+        r = http.get(
+            url,
+            headers=headers,
+        )
+
+        if r is None:
+            raise Exception("None response from hr core api get microsoft alias by employee id")
+        
+        if r.status_code == 401:
+            raise Exception(f"Unauthorized status when requesting to {url}")
+
+        if r.status_code == 404:
+            raise Exception(f"Could not find user with employee id {employee_id}")
+
+        return r.json()["emailAlias"]
+
+    
+    def __get_hr_api_bearer_token(self, target_client_id) -> str:
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "scope": f"{target_client_id}/.default",
+        }
+        r = http.post(
+            f"https://login.microsoftonline.com/{self.azure_tenant_id}/oauth2/v2.0/token",
+            data,
+        )
+        token = r.json()["access_token"]
+        return f"Bearer {token}"
+
---
+++ src/validate_issue.py
@@ -0,0 +1,39 @@
+import logging
+import re
+import sys
+from hr_core import HrCore
+
+logger = logging.getLogger("uvicorn.error")
+
+hr_core = HrCore()
+
+def validate(body):
+    try:
+        logger.info(f"[DualAccessValidator] Validating issue")
+
+        body = re.sub(r'\n+', '\n', body).strip()
+
+        split = body.split('\n')
+        employee_id_provided = ""
+        alias_provided = ""
+        for i in range(len(split) - 1):
+            v = split[i]
+            if "Microsoft Employee ID / PERN".lower() in v.lower():
+                employee_id_provided = split[i+1].lstrip("0") 
+            if "Microsoft Alias".lower() in v.lower():
+                alias_provided = split[i+1]
+        
+        if not employee_id_provided or not alias_provided:
+            raise Exception("mal-formatted issue")
+
+        employee_id = hr_core.get_employee_id_by_alias(alias_provided)
+
+        if employee_id != employee_id_provided:
+            raise Exception("Employee id does not match alias, it should instead be " + employee_id)
+
+        return "matches"
+    except Exception as e:
+        logger.error(f"[DualAccessValidator] error validating issue: {e}")
+        raise e
+
+validate(sys.argv[1])
