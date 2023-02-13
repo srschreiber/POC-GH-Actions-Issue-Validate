544ddcf25b571709636f120dabe6e5fe1ac618af
Sam Schreiber <srschreiber@github.com>
2023-02-13 14:47:03 -0800

    use step level instead of job level

--- src/validate_issue.py
+++ src/validate_issue.py
@@ -35,6 +35,8 @@ def validate(body):
         return "matches"
     except Exception as e:
         os.environ["ISSUE_DETECTED"] = "yes"
+        print(f"::set-env name=ISSUE_DETECTED::yes")
+
         logger.error(f"[DualAccessValidator] error validating issue: {e}")
 
 validate(sys.argv[1])
